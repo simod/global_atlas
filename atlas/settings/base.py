f168c13431aedb10beef9f9f41582d52d52f72ce
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-01-09 16:16:34 +0100

    add travis conf

--- atlas/settings/base.py
+++ atlas/settings/base.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'atlas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'atlas',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'PORT': 5432
     }
 }
 
