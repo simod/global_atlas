14dfaccc2c39c990e4e16e47f93ad7c4f9aa548b
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-01-16 11:16:04 +0100

    complete the api tests

--- maps/tests/test_api.py
+++ maps/tests/test_api.py
@@ -410,3 +410,477 @@ class CountryApiTests(ResourceTestCase):
             self.detail_url % co.pk, format='json'))
         self.assertEqual(Country.objects.count(), 2)
 
+
+class FormatApiTests(ResourceTestCase):
+    """Tests the Format apis"""
+
+    def setUp(self):
+        super(FormatApiTests, self).setUp()
+
+        load_test_data('formats')
+        self.list_url = '/api/formats/'
+        self.detail_url = '/api/formats/%s/'
+
+    def test_format_get_list_unauth(self):
+        """Test that a 401 is given when unauth"""
+        self.assertHttpUnauthorized(self.api_client.get(
+            self.list_url))
+
+    def test_format_get_list(self):
+        """Test that a valid json response is given
+        and the count is 2"""
+        self.api_client.client.login(username=username, password=password)
+        resp = self.api_client.get(self.list_url)
+        self.assertValidJSONResponse(resp)
+
+        self.assertEquals(len(self.deserialize(resp)['objects']), 2)
+
+    def test_format_get_detail_unauth(self):
+        """Test that a 401 is given on detail view when unath"""
+        co = Format.objects.all()[0]
+        self.assertHttpUnauthorized(
+            self.api_client.get(self.detail_url % co.pk))
+
+    def test_format_get_detail(self):
+        """Test that a valid format detail is returned when authenticated"""
+        self.api_client.client.login(username=username, password=password)
+        co = Format.objects.all()[0]
+        resp = self.api_client.get(self.detail_url % co.pk)
+        self.assertValidJSONResponse(resp)
+
+        self.assertKeys(self.deserialize(resp), [u'id', u'name', 
+            u'resolution', u'resource_uri'])
+        self.assertEqual(self.deserialize(resp)['name'], 'jpg')
+
+    def test_format_post_unauth(self):
+        """Test that 401 on unauthenticated post"""
+        self.assertHttpUnauthorized(self.api_client.post(self.list_url, 
+            data={}))
+
+    def test_format_post(self):
+        """Test a post while authenticated"""
+        self.assertEqual(Format.objects.count(), 2)
+        post_data = {
+            'name': 'gif',
+            'resolution': 'low'
+        }
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpCreated(self.api_client.post(self.list_url, 
+            format='json', data=post_data))
+        self.assertEqual(Format.objects.count(), 3)
+
+    def test_format_put_unauth(self):
+        """Test that 401 on unauthenticated put"""
+        self.assertHttpUnauthorized(self.api_client.put(self.list_url, 
+            data={}))
+
+    def test_format_put(self):
+        """Test a put while authenticated"""
+        co = Format.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        co_data = self.deserialize(self.api_client.get(
+            self.detail_url % co.pk))
+        new_data = co_data.copy()
+        new_data['name'] = 'jpeg'
+
+        self.assertEqual(Format.objects.count(), 2)
+        self.assertHttpAccepted(self.api_client.put(
+            self.detail_url % co.pk, format='json', data=new_data))
+        self.assertEqual(Format.objects.count(), 2)
+
+        self.assertEqual(Format.objects.get(pk=co.pk).name, 
+            'jpeg')
+
+    def test_format_delete_unauth(self):
+        """Test that 401 on unauthenticated delete"""
+        self.assertHttpUnauthorized(self.api_client.delete(
+            self.detail_url % 1, format='json'))
+
+    def test_format_delete(self):
+        """Test a delete while authenticated"""
+        self.assertEqual(Format.objects.count(), 2)
+        co = Format.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpAccepted(self.api_client.delete(
+            self.detail_url % co.pk, format='json'))
+        self.assertEqual(Format.objects.count(), 1)
+
+
+class MapSizeApiTests(ResourceTestCase):
+    """Tests the MapSize apis"""
+
+    def setUp(self):
+        super(MapSizeApiTests, self).setUp()
+
+        load_test_data('sizes')
+        self.list_url = '/api/sizes/'
+        self.detail_url = '/api/sizes/%s/'
+
+    def test_mapsize_get_list_unauth(self):
+        """Test that a 401 is given when unauth"""
+        self.assertHttpUnauthorized(self.api_client.get(
+            self.list_url))
+
+    def test_mapsize_get_list(self):
+        """Test that a valid json response is given
+        and the count is 2"""
+        self.api_client.client.login(username=username, password=password)
+        resp = self.api_client.get(self.list_url)
+        self.assertValidJSONResponse(resp)
+
+        self.assertEquals(len(self.deserialize(resp)['objects']), 2)
+
+    def test_mapsize_get_detail_unauth(self):
+        """Test that a 401 is given on detail view when unath"""
+        co = MapSize.objects.all()[0]
+        self.assertHttpUnauthorized(
+            self.api_client.get(self.detail_url % co.pk))
+
+    def test_mapsize_get_detail(self):
+        """Test that a valid format detail is returned when authenticated"""
+        self.api_client.client.login(username=username, password=password)
+        co = MapSize.objects.all()[0]
+        resp = self.api_client.get(self.detail_url % co.pk)
+        self.assertValidJSONResponse(resp)
+
+        self.assertKeys(self.deserialize(resp), [u'id', u'name', 
+            u'dimensions', u'resource_uri'])
+        self.assertEqual(self.deserialize(resp)['name'], 'A4')
+
+    def test_mapsize_post_unauth(self):
+        """Test that 401 on unauthenticated post"""
+        self.assertHttpUnauthorized(self.api_client.post(self.list_url, 
+            data={}))
+
+    def test_mapsize_post(self):
+        """Test a post while authenticated"""
+        self.assertEqual(MapSize.objects.count(), 2)
+        post_data = {
+            'name': 'A2',
+            'dimensions': '450*450'
+        }
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpCreated(self.api_client.post(self.list_url, 
+            format='json', data=post_data))
+        self.assertEqual(MapSize.objects.count(), 3)
+
+    def test_mapsize_put_unauth(self):
+        """Test that 401 on unauthenticated put"""
+        self.assertHttpUnauthorized(self.api_client.put(self.list_url, 
+            data={}))
+
+    def test_mapsize_put(self):
+        """Test a put while authenticated"""
+        co = MapSize.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        co_data = self.deserialize(self.api_client.get(
+            self.detail_url % co.pk))
+        new_data = co_data.copy()
+        new_data['name'] = 'A1'
+
+        self.assertEqual(MapSize.objects.count(), 2)
+        self.assertHttpAccepted(self.api_client.put(
+            self.detail_url % co.pk, format='json', data=new_data))
+        self.assertEqual(MapSize.objects.count(), 2)
+
+        self.assertEqual(MapSize.objects.get(pk=co.pk).name, 
+            'A1')
+
+    def test_mapsize_delete_unauth(self):
+        """Test that 401 on unauthenticated delete"""
+        self.assertHttpUnauthorized(self.api_client.delete(
+            self.detail_url % 1, format='json'))
+
+    def test_mapsize_delete(self):
+        """Test a delete while authenticated"""
+        self.assertEqual(MapSize.objects.count(), 2)
+        co = MapSize.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpAccepted(self.api_client.delete(
+            self.detail_url % co.pk, format='json'))
+        self.assertEqual(MapSize.objects.count(), 1)
+
+
+class CategoryApiTests(ResourceTestCase):
+    """Tests the Category apis"""
+
+    def setUp(self):
+        super(CategoryApiTests, self).setUp()
+
+        load_test_data('categories')
+        self.list_url = '/api/categories/'
+        self.detail_url = '/api/categories/%s/'
+
+    def test_category_get_list_unauth(self):
+        """Test that a 401 is given when unauth"""
+        self.assertHttpUnauthorized(self.api_client.get(
+            self.list_url))
+
+    def test_category_get_list(self):
+        """Test that a valid json response is given
+        and the count is 2"""
+        self.api_client.client.login(username=username, password=password)
+        resp = self.api_client.get(self.list_url)
+        self.assertValidJSONResponse(resp)
+
+        self.assertEquals(len(self.deserialize(resp)['objects']), 2)
+
+    def test_category_get_detail_unauth(self):
+        """Test that a 401 is given on detail view when unath"""
+        co = Category.objects.all()[0]
+        self.assertHttpUnauthorized(
+            self.api_client.get(self.detail_url % co.pk))
+
+    def test_category_get_detail(self):
+        """Test that a valid category detail is returned when authenticated"""
+        self.api_client.client.login(username=username, password=password)
+        co = Category.objects.all()[0]
+        resp = self.api_client.get(self.detail_url % co.pk)
+        self.assertValidJSONResponse(resp)
+
+        self.assertKeys(self.deserialize(resp), [u'id', u'name', 
+            u'slug', u'resource_uri'])
+        self.assertEqual(self.deserialize(resp)['slug'], 'location')
+
+    def test_category_post_unauth(self):
+        """Test that 401 on unauthenticated post"""
+        self.assertHttpUnauthorized(self.api_client.post(self.list_url, 
+            data={}))
+
+    def test_category_post(self):
+        """Test a post while authenticated"""
+        self.assertEqual(Category.objects.count(), 2)
+        post_data = {
+            'name': 'Damaged buildings',
+            'slug': 'damaged-buildings'
+        }
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpCreated(self.api_client.post(self.list_url, 
+            format='json', data=post_data))
+        self.assertEqual(Category.objects.count(), 3)
+
+    def test_category_put_unauth(self):
+        """Test that 401 on unauthenticated put"""
+        self.assertHttpUnauthorized(self.api_client.put(self.list_url, 
+            data={}))
+
+    def test_category_put(self):
+        """Test a put while authenticated"""
+        co = Category.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        co_data = self.deserialize(self.api_client.get(
+            self.detail_url % co.pk))
+        new_data = co_data.copy()
+        new_data['slug'] = 'loca'
+
+        self.assertEqual(Category.objects.count(), 2)
+        self.assertHttpAccepted(self.api_client.put(
+            self.detail_url % co.pk, format='json', data=new_data))
+        self.assertEqual(Category.objects.count(), 2)
+
+        self.assertEqual(Category.objects.get(pk=co.pk).slug, 
+            'loca')
+
+    def test_category_delete_unauth(self):
+        """Test that 401 on unauthenticated delete"""
+        self.assertHttpUnauthorized(self.api_client.delete(
+            self.detail_url % 1, format='json'))
+
+    def test_category_delete(self):
+        """Test a delete while authenticated"""
+        self.assertEqual(Category.objects.count(), 2)
+        co = Category.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpAccepted(self.api_client.delete(
+            self.detail_url % co.pk, format='json'))
+        self.assertEqual(Category.objects.count(), 1)
+
+
+class RequesterApiTests(ResourceTestCase):
+    """Tests the Requester apis"""
+
+    def setUp(self):
+        super(RequesterApiTests, self).setUp()
+
+        load_test_data('requesters')
+        self.list_url = '/api/requesters/'
+        self.detail_url = '/api/requesters/%s/'
+
+    def test_requester_get_list_unauth(self):
+        """Test that a 401 is given when unauth"""
+        self.assertHttpUnauthorized(self.api_client.get(
+            self.list_url))
+
+    def test_requester_get_list(self):
+        """Test that a valid json response is given
+        and the count is 2"""
+        self.api_client.client.login(username=username, password=password)
+        resp = self.api_client.get(self.list_url)
+        self.assertValidJSONResponse(resp)
+
+        self.assertEquals(len(self.deserialize(resp)['objects']), 2)
+
+    def test_requester_get_detail_unauth(self):
+        """Test that a 401 is given on detail view when unath"""
+        co = Requester.objects.all()[0]
+        self.assertHttpUnauthorized(
+            self.api_client.get(self.detail_url % co.pk))
+
+    def test_requester_get_detail(self):
+        """Test that a valid requester detail is returned when authenticated"""
+        self.api_client.client.login(username=username, password=password)
+        co = Requester.objects.all()[0]
+        resp = self.api_client.get(self.detail_url % co.pk)
+        self.assertValidJSONResponse(resp)
+
+        self.assertKeys(self.deserialize(resp), [u'id', u'name', 
+            u'slug', u'resource_uri'])
+        self.assertEqual(self.deserialize(resp)['slug'], 'johnny')
+
+    def test_requester_post_unauth(self):
+        """Test that 401 on unauthenticated post"""
+        self.assertHttpUnauthorized(self.api_client.post(self.list_url, 
+            data={}))
+
+    def test_requester_post(self):
+        """Test a post while authenticated"""
+        self.assertEqual(Requester.objects.count(), 2)
+        post_data = {
+            'name': 'Joey',
+            'slug': 'joey'
+        }
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpCreated(self.api_client.post(self.list_url, 
+            format='json', data=post_data))
+        self.assertEqual(Requester.objects.count(), 3)
+
+    def test_requester_put_unauth(self):
+        """Test that 401 on unauthenticated put"""
+        self.assertHttpUnauthorized(self.api_client.put(self.list_url, 
+            data={}))
+
+    def test_requester_put(self):
+        """Test a put while authenticated"""
+        co = Requester.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        co_data = self.deserialize(self.api_client.get(
+            self.detail_url % co.pk))
+        new_data = co_data.copy()
+        new_data['slug'] = 'john'
+
+        self.assertEqual(Requester.objects.count(), 2)
+        self.assertHttpAccepted(self.api_client.put(
+            self.detail_url % co.pk, format='json', data=new_data))
+        self.assertEqual(Requester.objects.count(), 2)
+
+        self.assertEqual(Requester.objects.get(pk=co.pk).slug, 
+            'john')
+
+    def test_requester_delete_unauth(self):
+        """Test that 401 on unauthenticated delete"""
+        self.assertHttpUnauthorized(self.api_client.delete(
+            self.detail_url % 1, format='json'))
+
+    def test_requester_delete(self):
+        """Test a delete while authenticated"""
+        self.assertEqual(Requester.objects.count(), 2)
+        co = Requester.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpAccepted(self.api_client.delete(
+            self.detail_url % co.pk, format='json'))
+        self.assertEqual(Requester.objects.count(), 1)
+
+class SourceApiTests(ResourceTestCase):
+    """Tests the Source apis"""
+
+    def setUp(self):
+        super(SourceApiTests, self).setUp()
+
+        load_test_data('sources')
+        self.list_url = '/api/sources/'
+        self.detail_url = '/api/sources/%s/'
+
+    def test_source_get_list_unauth(self):
+        """Test that a 401 is given when unauth"""
+        self.assertHttpUnauthorized(self.api_client.get(
+            self.list_url))
+
+    def test_source_get_list(self):
+        """Test that a valid json response is given
+        and the count is 2"""
+        self.api_client.client.login(username=username, password=password)
+        resp = self.api_client.get(self.list_url)
+        self.assertValidJSONResponse(resp)
+
+        self.assertEquals(len(self.deserialize(resp)['objects']), 2)
+
+    def test_source_get_detail_unauth(self):
+        """Test that a 401 is given on detail view when unath"""
+        co = Source.objects.all()[0]
+        self.assertHttpUnauthorized(
+            self.api_client.get(self.detail_url % co.pk))
+
+    def test_source_get_detail(self):
+        """Test that a valid requester detail is returned when authenticated"""
+        self.api_client.client.login(username=username, password=password)
+        co = Source.objects.all()[0]
+        resp = self.api_client.get(self.detail_url % co.pk)
+        self.assertValidJSONResponse(resp)
+
+        self.assertKeys(self.deserialize(resp), [u'id', u'name', 
+            u'slug', u'resource_uri'])
+        self.assertEqual(self.deserialize(resp)['slug'], 'jrc')
+
+    def test_source_post_unauth(self):
+        """Test that 401 on unauthenticated post"""
+        self.assertHttpUnauthorized(self.api_client.post(self.list_url, 
+            data={}))
+
+    def test_source_post(self):
+        """Test a post while authenticated"""
+        self.assertEqual(Source.objects.count(), 2)
+        post_data = {
+            'name': 'World Bank',
+            'slug': 'wb'
+        }
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpCreated(self.api_client.post(self.list_url, 
+            format='json', data=post_data))
+        self.assertEqual(Source.objects.count(), 3)
+
+    def test_source_put_unauth(self):
+        """Test that 401 on unauthenticated put"""
+        self.assertHttpUnauthorized(self.api_client.put(self.list_url, 
+            data={}))
+
+    def test_source_put(self):
+        """Test a put while authenticated"""
+        co = Source.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        co_data = self.deserialize(self.api_client.get(
+            self.detail_url % co.pk))
+        new_data = co_data.copy()
+        new_data['slug'] = 'eu-jrc'
+
+        self.assertEqual(Source.objects.count(), 2)
+        self.assertHttpAccepted(self.api_client.put(
+            self.detail_url % co.pk, format='json', data=new_data))
+        self.assertEqual(Source.objects.count(), 2)
+
+        self.assertEqual(Source.objects.get(pk=co.pk).slug, 
+            'eu-jrc')
+
+    def test_source_delete_unauth(self):
+        """Test that 401 on unauthenticated delete"""
+        self.assertHttpUnauthorized(self.api_client.delete(
+            self.detail_url % 1, format='json'))
+
+    def test_source_delete(self):
+        """Test a delete while authenticated"""
+        self.assertEqual(Source.objects.count(), 2)
+        co = Source.objects.all()[0]
+        self.api_client.client.login(username=username, password=password)
+        self.assertHttpAccepted(self.api_client.delete(
+            self.detail_url % co.pk, format='json'))
+        self.assertEqual(Source.objects.count(), 1)
+
