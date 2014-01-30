11be92d1c6d970eda1919d6144bea946a0a0ae6c
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-01-30 14:54:37 +0100

    add map layer

--- static/app/scripts/controllers/map_controller.js
+++ static/app/scripts/controllers/map_controller.js
@@ -5,8 +5,27 @@
 
   var module = angular.module('map_controller', ['leaflet-directive']);
 
-  module.controller('MapController', function($scope){
-
+  module.controller('MapController', function($scope, $http, DataUrls){
+    angular.extend($scope, {
+      center: {
+        lat: 5.6,
+        lng: 3.9,
+        zoom: 2
+      }
+    });
+    $http.get(DataUrls.countries).success(function(data){
+      angular.extend($scope, {
+        countries: {
+          data: data,
+          style: {
+            fillColor: 'grey',
+            weight: 0.7,
+            opacity: 1,
+            color: 'white',
+            fillOpacity: 0.9
+          }
+        }
+      });
+    });
   });
-
 })();
