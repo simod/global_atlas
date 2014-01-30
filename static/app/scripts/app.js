6e74f736a6f409b2df3e08a7ff1e0072b4291359
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-01-30 11:51:25 +0100

    add basic map

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -5,6 +5,7 @@
   goog.require('atlas_main_controller');
   goog.require('atlas_mr_controller');
   goog.require('country_select_controller');
+  goog.require('map_controller');
 
   var module = angular.module('atlas', [
     'ngCookies',
@@ -12,6 +13,7 @@
     'ngRoute',
     'atlas_main_controller',
     'atlas_mr_controller',
-    'country_select_controller'
+    'country_select_controller',
+    'map_controller'
   ]);
 })(); 
---
+++ static/app/scripts/controllers/map_controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function(){
+  goog.provide('map_controller');
+
+  var module = angular.module('map_controller', ['leaflet-directive']);
+
+  module.controller('MapController', function($scope){
+
+  });
+
+})();
