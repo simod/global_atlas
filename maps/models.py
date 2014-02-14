fd9014ad45d0c1c18bb6e9cb6bccacde532c82c0
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-02-14 12:24:40 +0100

    increase the map title length

--- maps/models.py
+++ maps/models.py
@@ -22,7 +22,7 @@ class CountryManager(models.Manager):
 
 class Map(geomodels.Model):
     """Map object"""
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=255)
     date = models.DateField()
     theme = models.ForeignKey('Theme')
     category = models.ForeignKey('Category')
